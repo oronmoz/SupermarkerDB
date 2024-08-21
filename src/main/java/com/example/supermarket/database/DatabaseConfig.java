fa3f76bf3da8a80d99d555d35528b1971d144c75
oronmoz <167132613+oronmoz@users.noreply.github.com>
2024-08-22 02:52:53 +0300

    Update DatabaseConfig.java

--- src/main/java/com/example/supermarket/database/DatabaseConfig.java
+++ src/main/java/com/example/supermarket/database/DatabaseConfig.java
@@ -2,7 +2,7 @@ package com.example.supermarket.database;
 
 public class DatabaseConfig {
     public static final String URL = "jdbc:postgresql://localhost:5432/supermarket";
-    public static final String USER = "postgres";
-    public static final String PASSWORD = "1234";
+    public static final String USER = "YOUR_USERNAME";
+    public static final String PASSWORD = "YOUR_PASSWORD";
     public static final String DRIVER = "org.postgresql.Driver";
 }
